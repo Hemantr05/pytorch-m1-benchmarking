c26e320f84338c55d67f270776afcf9a07ab67d5
hemantr05 <basketballlife600@gmail.com>
2022-11-11 04:20:26 +0530

    nomenclature change

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ mlflow.pytorch.autolog()
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-parser.add_argument('--batch_size', type=int, default=64, metavar='N',
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
